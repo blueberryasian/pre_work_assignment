62f441e80f7e31488e608bb38a8f4ba941458b76
blueberryasian <140463689+blueberryasian@users.noreply.github.com>
2023-07-24 18:17:57 -0700

    Delete Chapter 6 Python coding directory

--- Chapter 6 Python coding/Favorite_placesTIY.py
+++
@@ -1,9 +0,0 @@
-favorite_places = {
-    'sean': ['mexico' , 'utah', 'idaho'],
-        'jelly': ['my bed' ,'her bed' , 'airbnb bed'],
-            'colin': ['hawaii', 'ohio' , 'michigan']
-}
-
-for name, place in favorite_places.items():
-    print(name + ' loves ')
-    print(place)
--- Chapter 6 Python coding/PersonTIY.py
+++
@@ -1,7 +0,0 @@
-jelly = {
-    'first_name': 'jehliza' ,
-          'last_name': 'francisco',
-           'age': 18,
-            'city': 'poway' 
-        }
-print(jelly)
