30c8034c95850b9906978e9d933b382c62863209
blueberryasian <140463689+blueberryasian@users.noreply.github.com>
2023-07-24 18:18:25 -0700

    Delete Proper coding directory

--- Proper coding/avoiding_index_errors_in_lists.py
+++
@@ -1,5 +0,0 @@
-#This error occurs because python lists start at 0 
-motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles[2])
-motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles[-1])
